30e47154a167f637178f90582b61c02f6d0316e5
rsleggett <rsleggett@googlemail.com>
2012-05-16 15:20:47 +0100

    Initial commmit

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-tridion-mirror
-==============
+#CodeMirror Tridion GUI Extension
 
-CodeMirror Tridion GUI Extension
+Purpose: Add syntax highlighting to the source tab of a Template Building Block View in SDL Tridion 2011.
+Version 0.1 ALPHA
+Author: Robert Stevenson-Leggett  
+Supports: SDL Tridion 2011  
+
+Uses the fantasic Code Mirror: http://codemirror.net/
+
+**Please note this is an Alpha / Proof of Concept**
+
+##Installation Instructions
+
+ 1. On build of the project, there is a post build event which copies the needed files to C:\Extensions\CodeMirrorExension
+ 2. Take the files in that folder and put them on the Conent Management Server in a folder of your choosing.
+ 3. In IIS, create a virtual directory under the %SDL Tridion 2011 Website%\Editors\ called CodeMirror pointing to the directory from Step 2.
+ 4. Add the following lines to the config in %TridionDir%\web\WebUI\WebRoot\Configuration\System.config
+ 
+    <editor name="CodeMirrorExtension" xmlns="http://www.sdltridion.com/2009/GUI/Configuration">
+		<installpath xmlns="http://www.sdltridion.com/2009/GUI/Configuration">C:\CodeMirrorExtension\</installpath>
+		<configuration xmlns="http://www.sdltridion.com/2009/GUI/Configuration">CodeMirrorExtension.config</configuration>
+		<vdir xmlns="http://www.sdltridion.com/2009/GUI/Configuration">CodeMirror</vdir>
+	</editor>
+	
+ 5. Go to your SDL Tridion 2011 URL and hit Ctrl+F5 to ensure the cache is clear.
+ 6. Open a Template Building Block, all being well you should see a new 'Enable Code Mirror' button on the Ribbon Toolbar.
+ 7. Switch to the source tab and click 'Enable Code Mirror' to begin editting! (P.S You can now use the TAB key as it was intended!)
+ 
+## Known issues
+
+ In this Alpha version, enabling the Code Mirror button while not on the Source Tab will cause the code in the Template Building Block to be hidden from view. Don't panic, close the item without saving and re open.
---
+++ src/BuildingBlocks.Tridion2011Extensions.CodeMirror/BuildingBlocks.Tridion2011Extensions.CodeMirror/Scripts/tridion-code-mirror-extension.js
@@ -0,0 +1,64 @@
+﻿/*!
+    Tridion Code Mirror Extension
+
+    Copyright (C) 2012 Robert Stevenson-Leggett
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+    Version: 0.1
+    Author: Robert Stevenson-Leggett
+*/
+
+Type.registerNamespace("Rob.Prototype.CodemirrorExtensions");
+
+Rob.Prototype.CodemirrorExtensions.EnableCodeMirror = function Rob$Prototype$CodemirrorExtensions$EnableCodemirror() {
+    console.log('ctor');
+    Type.enableInterface(this, "Rob.Prototype.CodemirrorExtensions.EnableCodeMirror");
+    this.addInterface("Tridion.Cme.Command", ["EnableCodeMirror"]);
+    this.HasExecuted = false;
+    this.CodeArea = null;
+};
+
+Rob.Prototype.CodemirrorExtensions.EnableCodeMirror.prototype.isAvailable = function EnableCodeMirror$isAvailable(selection) {
+    console.log('is available');
+    return true;
+};
+
+Rob.Prototype.CodemirrorExtensions.EnableCodeMirror.prototype.isEnabled = function EnableCodeMirror$isEnabled(selection) {
+    console.log('is enabled');
+    return true;
+};
+
+Rob.Prototype.CodemirrorExtensions.EnableCodeMirror.prototype._execute = function EnableCodeMirror$_execute(selection) {
+
+    if (this.HasExecuted) {
+        this.CodeArea.toTextArea();
+        this.HasExecuted = false;
+        this.CodeArea = null;
+        $('.EnableCodeMirror .text').textContent = 'Enable Code Mirror';
+        return;
+    }
+
+    var codeArea = document.getElementById("SourceArea");
+
+    this.CodeArea = CodeMirror.fromTextArea(codeArea, {
+        lineNumbers: true,
+        matchBrackets: true,
+        mode: { name: "xml", htmlMode: true },
+        indentUnit: 4,
+        indentWithTabs: true,
+        enterMode: "keep",
+        tabMode: "shift",
+        onChange: function (editor) {
+            editor.save();
+        }
+    });
+
+    $('.EnableCodeMirror .text').textContent  = 'Disable Code Mirror';
+    this.HasExecuted = true;
+};
+
